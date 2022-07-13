9b08724f20570e5892f606c22741c54c508dddf3
senryx <max001554@mail.ru>
2022-07-13 17:11:12 +0400

    asd

--- site/footballnews/blog/forms.py
+++ site/footballnews/blog/forms.py
@@ -2,23 +2,23 @@ from django import forms
 from .models import *
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+# class AddPostForm(forms.Form):
+#     title = forms.CharField(max_length=255)
+#     slug = forms.SlugField(max_length=255)
+#     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+#     is_published = forms.BooleanField()
+#     cat = forms.ModelChoiceField(queryset=Category.objects.all())
 
 
-# class AddPostForm(forms.ModelForm):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['cat'].empty_label = 'Категория не выбрана'
-#
-#     class Meta:
-#         model = Post
-#         field = ['title', 'slug', 'content', 'photo' 'is_published', 'cat']
-#         widgets = {
-#             'title': forms.TextInput(attrs={'class': 'form-input'}),
-#             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
-#         }
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
+
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
